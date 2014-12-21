647df6512ec608702f756ce56a5b3d5f1bdfc140
seb255 <sebastien.brogly@free.fr>
2014-12-21 07:58:49 +0100

    Update makeCacheMatrix.R
    Add of a few more comment to permitright understanding of the stuff proposed here

--- makeCacheMatrix.R
+++ makeCacheMatrix.R
@@ -1,3 +1,6 @@
+## This function define the function necessary to manage matrix creation and it inverse calculation saving
+## in a variable in a different environment than the function, so will permit to keep the value outside of the function
+
 makeCacheMatrix <- function(x=matrix()){
 	m <- NULL
 	set <- function(y){  				##set the value of the matrix
@@ -7,7 +10,7 @@ makeCacheMatrix <- function(x=matrix()){
 	get <-function() x				##get the value of the matrix
 	setinv <- function(inv) m <<-inv		##set the value of the inverse
 	getinv <- function() m				##get the valeur of the inverse
-	list(set=set,get=get,
+	list(set=set,get=get,				##give the list of accessible defined functions from outside
 		setinv = setinv,
 		getinv = getinv)
 }
